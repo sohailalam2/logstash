7628719b535a8da55ce7375e3d0f3a5df7df5ccb
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-05-17 16:13:46 -0400

    Changes in JRuby 9k when handling Time
    Becore JRuby 9K, the time instance was using Joda time, this was forcing
    the time to only show milliseconds precision, with the change to 9K they
    switch their implementation to java.time which support nanoseconds
    precision. But if you are using DateTime, it will still use joda time
    and be limited to milliseconds precision.
    
    Since internally we also use joda time we are also limited with
    milliseconds precision, to support nanoseconds we will need to refactor
    our timestamp class.
    
    This commits change the expectation to only expect milliseconds
    precision.

--- logstash-core/spec/logstash/legacy_ruby_timestamp_spec.rb
+++ logstash-core/spec/logstash/legacy_ruby_timestamp_spec.rb
@@ -96,7 +96,7 @@ describe LogStash::Timestamp do
   end
 
   context "numeric casting methods" do
-    let (:now) {Time.now}
+    let (:now) {DateTime.now.to_time}
     subject { LogStash::Timestamp.new(now) }
 
     it "should support to_i" do
